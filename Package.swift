13a9b8942efeb7eb26ee8d00d3e8152876b608e3
AginS <agins.main@gmail.com>
2022-12-26 01:05:52 +0300

    added Base32 dependency

--- Package.swift
+++ Package.swift
@@ -20,13 +20,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.20.1"),
+        .package(url: "https://github.com/norio-nomura/Base32", .upToNextMajor(from: "0.9.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GAuthDecrypt",
-            dependencies: [ .product(name: "SwiftProtobuf", package: "swift-protobuf") ]),
+            dependencies: [ .product(name: "SwiftProtobuf", package: "swift-protobuf"), .product(name: "Base32", package: "Base32") ]),
         .testTarget(
             name: "GAuthDecryptTests",
             dependencies: ["GAuthDecrypt"]),
