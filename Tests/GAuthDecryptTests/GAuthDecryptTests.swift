981fe5fe9bd1624a25dd8ad540c09ada911745c1
AginS <agins.main@gmail.com>
2022-12-26 00:11:55 +0300

    added multiple decrypt test

--- Tests/GAuthDecryptTests/GAuthDecryptTests.swift
+++ Tests/GAuthDecryptTests/GAuthDecryptTests.swift
@@ -19,6 +19,15 @@ final class GAuthDecryptTests: XCTestCase {
         let gauth = GAuthDecryptFrom(string: "otpauth-migration://offline?data=CjEKCkhlbGxvId6tvu8SGEV4YW1wbGU6YWxpY2VAZ29vZ2xlLmNvbRoHRXhhbXBsZTAC")
         XCTAssert(gauth?.first?.name == "Example:alice@google.com")
     }
+
+    func testGAuthMultipleDecrypt() throws {
+        let gauth = GAuthDecryptFrom(string: "otpauth-migration://offline?data=CiMKC5OYSRBRJMThkOSJEg5Tb21lIGNvZGUgd2l0aCABKAEwAgoZCgqHIGPETdC5oJg5EgV0ZXN0MSABKAEwAgoZCgqHIGPETdC5oJg5EgV0ZXN0MiABKAEwAhABGAEgAA%3D%3D")
+        if (gauth == nil) {
+            XCTFail("Cannot decrypt from multiple import")
+            return
+        }
+        XCTAssert(gauth!.count == 3)
+    }
     
     func testIncorrectGAuthDecryptFrom() throws {
         let gauth = GAuthDecryptFrom(string: "otpauth-migration://offline?data=CjEKCkhlbdsdGxvId6tvu8SGEV4YW1wbGU6YWxpY2VAZ29vZ2xlLmNvbRoHRXhhbXBsZTAC")
